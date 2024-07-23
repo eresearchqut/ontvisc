cee15274ea11dec9d0612418b5ef4af539c2ec44
maelyg <marieemilie.gauthier@qut.edu.au>
2024-07-23 14:45:29 +1000

    remove decimal points for taxon_ids

--- bin/summary_read_classification.py
+++ bin/summary_read_classification.py
@@ -22,6 +22,7 @@ def main():
     for kaiju_file in glob.glob('*_kaiju_summary_viral.tsv'):
         kaiju_df = pd.read_csv(kaiju_file, sep="\t", index_col=False)
         kaiju_df["percent"] = pd.to_numeric(kaiju_df["percent"], errors='coerce', downcast="float")
+        kaiju_df["taxon_id"] = kaiju_df["taxon_id"].astype(int)
         kaiju_df_filtered = kaiju_df.drop(kaiju_df[kaiju_df["percent"] < 0.05].index).sort_values(by=['reads'], ascending=False)
         kaiju_df_html = kaiju_df.to_html(index=False).replace('<table border="1" class="dataframe">','<table class="table table-striped">') # use bootstrap styling
         kaiju_df_filtered_html = kaiju_df_filtered.to_html(index=False).replace('<table border="1" class="dataframe">','<table class="table table-striped">') # use bootstrap styling 
